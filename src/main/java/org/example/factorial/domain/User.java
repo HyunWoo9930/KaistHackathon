e6d2fc8e2ba4d7071f4fb498a2b0ad2403224521
wldh25 <liho858@gmail.com>
2024-07-29 07:57:27 +0900

    [ADD] 스웨거 설정 및 Documents domain & Listeners 추가 + User domain 추가

---
+++ src/main/java/org/example/factorial/domain/User.java
@@ -0,0 +1,34 @@
+package org.example.factorial.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.List;
+
+@Setter
+@Getter
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"username", "password", "nick_name"})})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(hidden = true)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String nickName;
+    private String profileUrl;
+    private int totalLikes;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private List<Documents> documents = new ArrayList<>();
+}
